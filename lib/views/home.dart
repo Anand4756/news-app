a1d7e643d1d855c973a2df3d247970fef200d9ec
Anandk <anand.k4756@gmail.com>
2022-09-10 20:46:14 +0530

    categories of news

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -8,45 +10,94 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List list = [];
+
+  _readdata() async {
+    await DefaultAssetBundle.of(context)
+        .loadString("assets/categories.json")
+        .then((value) => setState(() => list = json.decode(value)));
+    print(list);
+  }
+
+  void initState() {
+    _readdata();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Flutter"),
-              Text(
-                "News",
-                style: TextStyle(color: Colors.black),
-              )
-            ],
-          ),
-          elevation: 0.0,
+      backgroundColor: Colors.grey[350],
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Flutter"),
+            Text(
+              "News",
+              style: TextStyle(color: Colors.black),
+            )
+          ],
         ),
-        body: Container(
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: [Categorytile(), Categorytile(), Categorytile()],
+        elevation: 0.0,
+      ),
+      body: Container(
+          child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: SizedBox(
+              child: Text(
+                "Categories",
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ),
           ),
-        ));
+          Container(
+            child: Expanded(
+              child: ListView.builder(
+                itemCount: list.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, int index) {
+                  return Categorytile(
+                    imageurl: list[index]['imageurl'],
+                    categoryname: list[index]['title'],
+                  );
+                },
+                // ListTile(title: Text(list[index]['title']));
+                // children: [
+                //   Categorytile(),
+                //   Categorytile(),
+                //   Categorytile(),
+                //   Categorytile(),
+                //   Categorytile()
+                // ],
+              ),
+            ),
+          ),
+        ],
+      )),
+    );
   }
 }
 
 class Categorytile extends StatelessWidget {
   final imageurl, categoryname;
-  const Categorytile({Key? key, this.imageurl, this.categoryname})
-      : super(key: key);
+
+  const Categorytile({required this.imageurl, required this.categoryname});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Stack(children: [
-      Image.network(
-        'https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-        height: 150.0,
-      ),
-      Text("hii"),
-    ]));
+    return Padding(
+      padding: const EdgeInsets.only(left: 12.0, right: 12.0, top: 18.0),
+      child: Container(
+          child: Stack(children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(17),
+          child: Image.network(imageurl, width: 90.0),
+        ),
+        Text(categoryname),
+      ])),
+    );
   }
 }
